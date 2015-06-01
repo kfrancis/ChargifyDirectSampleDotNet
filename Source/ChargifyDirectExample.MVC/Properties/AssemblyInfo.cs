2fb4ad173efe80b1d5f8ebbb834c665fc3fb0b93
Kori Francis <byter@me.com>
2015-06-01 11:20:41 -0400

    Remove last remnants of MVC4

--- Source/ChargifyDirectExample.MVC/Properties/AssemblyInfo.cs
+++ Source/ChargifyDirectExample.MVC/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ChargifyDirectExample.MVC4")]
+[assembly: AssemblyTitle("ChargifyDirectExample.MVC")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ChargifyDirectExample.MVC4")]
+[assembly: AssemblyProduct("ChargifyDirectExample.MVC")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
